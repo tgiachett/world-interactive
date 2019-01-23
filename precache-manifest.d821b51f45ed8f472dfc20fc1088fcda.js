c793dad0f2d44549842a84380ca2b31102e3e3a2
Tobia <tgiachett@gmail.com>
2019-01-22 23:57:49 -0800

    Updates

---
+++ precache-manifest.d821b51f45ed8f472dfc20fc1088fcda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c66b5a780b7d40b6350",
+    "url": "/world-interactive/static/css/main.f7bfe6d3.chunk.css"
+  },
+  {
+    "revision": "7c66b5a780b7d40b6350",
+    "url": "/world-interactive/static/js/main.7c66b5a7.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/world-interactive/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "65f692df82ccb14f36d2",
+    "url": "/world-interactive/static/js/runtime~main.65f692df.js"
+  },
+  {
+    "revision": "4de019b4c01b2dbc711e2bdeb36d2ecf",
+    "url": "/world-interactive/index.html"
+  }
+];
